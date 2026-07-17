cd7c1bfdf220bfcbc19503968623716763f6e1b2
Fani Hernanda <nandaori04@gmail.com>
2026-07-17 12:38:31 +0800

    Fix EditTransactionScreen missing sp import

--- app/src/main/java/com/financeapp/ui/screens/EditTransactionScreen.kt
+++ app/src/main/java/com/financeapp/ui/screens/EditTransactionScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.financeapp.data.model.TransactionType
 import com.financeapp.ui.components.AmountInput
