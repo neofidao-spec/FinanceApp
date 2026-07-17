f8f0498684f83f7547707a7ba758c80202a98a4b
Fani Hernanda <nandaori04@gmail.com>
2026-07-17 10:54:24 +0800

    Fix Hilt plugin: declare versions in root build.gradle.kts
    - KSP 1.9.22-1.0.17 + Hilt 2.48.1 declared with 'apply false' in root
    - Remove resolutionStrategy from settings.gradle.kts
    - Standard Hilt + KSP setup pattern

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,16 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.devtools.ksp") {
-                useModule("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.17")
-            }
-            if (requested.id.id == "com.google.dagger.hilt.android") {
-                useModule("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
-            }
-        }
-    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
